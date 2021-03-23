68d8b713f2ed51453f162d0da20370099fea2fe8
Joshua Frazier <JoshuaSFrazier@hotmail.com>
2021-03-23 03:17:18 -0400

    pushing to Github

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,9 @@
 const LoadCart = () => {
     let products = JSON.parse(localStorage.getItem("productsInCart"));
     let content = "";
-    if (products == null) {
-        content += `<div id="">
+    if (products == "") {
+        alert("Empty Cart");
+        content += `<div id="emptyCart">
         <h2>There are no items in your cart</h2>
         <br>
         <a href="index.html"><p>Look at our selection of Teddies</p></a></div>`;
@@ -13,7 +14,7 @@ const LoadCart = () => {
             <img src="${Teddy.imageUrl}">
             <h3>Name: ${Teddy.name}</h3>
              <h3>Price: $${Teddy.price}</h3>
-             <h3>Quantity: ${Teddy.quantity}</h3>
+             <h3>Quantity: <button type="button" onclick="addQuantity('${Teddy._id}')">+</button>${Teddy.quantity}<button type="button" onclick="subQuantity('${Teddy._id}')">-</button></h3>
              </div>`;
             
         });
@@ -25,12 +26,28 @@ const LoadCart = () => {
 }
 LoadCart();
 
-const addQuantity = () => {
-
-
+const addQuantity = (productID) => {
+    let products = JSON.parse(localStorage.getItem("productsInCart"));
+    let index = products.findIndex(product  => product._id == productID);
+    if (index != -1) {
+        products[index].quantity +=1;    
+    } 
+    localStorage.setItem("productsInCart", JSON.stringify(products));
+    LoadCart();
+    
 }
 
-const subQuantity = () => {
-
-
+const subQuantity = (productID) => {
+    let products = JSON.parse(localStorage.getItem("productsInCart"));
+    let index = products.findIndex(product  => product._id == productID);
+    if (index != -1) {
+        products[index].quantity -=1; 
+        localStorage.setItem("productsInCart", JSON.stringify(products));
+        if (products[index].quantity == 0) {
+            alert("Item removed from cart")
+            products.splice(index, 1);
+            localStorage.setItem("productsInCart", JSON.stringify(products));
+        }   
+    } 
+    LoadCart();
 }
